fe8608cf76b0dcd51626e1f4b360d1437a44bd8d
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2022-01-14 22:17:20 -0500

    fix render issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
-import './App.css';
 
 function App() {
   const [categories] = useState([
@@ -28,8 +27,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
-        <About></About>
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
       </main>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
   const { categories = [], setCurrentCategory, currentCategory } = props;
 
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
     <header className='flex-row px-1'>
@@ -25,14 +26,12 @@ function Nav(props) {
               About me
             </a>
           </li>
-          <li>
-            <span>Contact</span>
+          <li className={'mx-2'}>
+            <span onClick={() => handleClick('Contact')}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1' ${
-                currentCategory.name === category.name && 'navActive'
-              }`}
+              className={`mx-1 ${currentCategory.name === category.name}`}
               key={category.name}
             >
               <span
